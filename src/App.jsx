64a1f7a1cff2533782c73565613b52c5d0dc81eb
Kyle Mendoza <kyle@monark.gg>
2024-09-01 02:48:05 +0800

    Added all main navigations and some animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import "./index.css";
 
 //Layouts
 import Navigation from "./layouts/Navigation";
+
 //Pages
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Tour from "./pages/Tour";
+import Shop from "./pages/Shop";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -14,6 +18,9 @@ function App() {
         <Route path="/" element={<Navigation />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="tour" element={<Tour />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/layouts/Navigation/index.jsx
+++ src/layouts/Navigation/index.jsx
@@ -1,19 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./index.module.scss";
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
+import clsx from "clsx";
 
-export default function index() {
+export default function Index() {
+  const location = useLocation();
+
   return (
     <div className={styles["main-container"]}>
       <div className={styles["navigation"]}>
         <div className={styles["navigation__nav"]}>
           <div className={styles["navigation__nav__logo"]}></div>
           <div className={styles["navigation__nav__links"]}>
-            <a href="/">Home</a>
-            <a href="tour">Virtual Tour</a>
-            <a href="/shop">Shop</a>
-            <a href="/about">About</a>
-            <a href="/contacts">Contacts</a>
+            <NavLink
+              to="/"
+              exact
+              className={clsx(
+                styles["nav-link"],
+                location.pathname === "/" && styles["active-link"]
+              )}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/tour"
+              className={clsx(
+                styles["nav-link"],
+                location.pathname === "/tour" && styles["active-link"]
+              )}
+            >
+              Virtual Tour
+            </NavLink>
+            <NavLink
+              to="/shop"
+              className={clsx(
+                styles["nav-link"],
+                location.pathname === "/shop" && styles["active-link"]
+              )}
+            >
+              Shop
+            </NavLink>
+            <NavLink
+              to="/about"
+              className={clsx(
+                styles["nav-link"],
+                location.pathname === "/about" && styles["active-link"]
+              )}
+            >
+              About
+            </NavLink>
+            <NavLink
+              to="/contact"
+              className={clsx(
+                styles["nav-link"],
+                location.pathname === "/contact" && styles["active-link"]
+              )}
+            >
+              Contacts
+            </NavLink>
           </div>
         </div>
       </div>
